39931d6815bd10aa2532524ec35259b6faea47ac
DerivadaDX <derivadadx@gmail.com>
2025-03-17 22:55:05 -0300

    Se agregó la propiedad `Valoraciones` en `Jugador` y el método `AgregarValoracion`.

--- src/Solver/Jugador.cs
+++ src/Solver/Jugador.cs
@@ -1,4 +1,5 @@
-﻿namespace Solver
+﻿
+namespace Solver
 {
     internal class Jugador
     {
@@ -8,5 +9,14 @@
         }
 
         internal int Id { get; }
+        internal List<Atomo> Valoraciones { get; } = [];
+
+        internal void AgregarValoracion(Atomo atomo)
+        {
+            if (Valoraciones.Any(v => v.Posicion == atomo.Posicion))
+                throw new ArgumentException($"Ya existe una valoración para el átomo en la posicíon {atomo.Posicion}");
+
+            Valoraciones.Add(atomo);
+        }
     }
 }
