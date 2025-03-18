478efb961d95471640e1930d29e33439170757cf
DerivadaDX <derivadadx@gmail.com>
2025-03-17 22:31:53 -0300

    Se agregó parámetro `valoracion` en `Atomo` y se agregaron tests.

--- src/Solver/Atomo.cs
+++ src/Solver/Atomo.cs
@@ -2,10 +2,16 @@
 {
     internal class Atomo
     {
-        public Atomo(int posicion)
+        public Atomo(int posicion, decimal valoracion)
         {
             if (posicion <= 0)
-                throw new ArgumentException("La posición debe ser positiva", nameof(posicion));
+                throw new ArgumentException($"La posición debe ser positiva: {posicion}", nameof(posicion));
+
+            if (valoracion < 0)
+                throw new ArgumentException($"La valoración no puede ser negativa: {valoracion}", nameof(valoracion));
+
+            if (valoracion > 1)
+                throw new ArgumentException($"La valoración no puede ser mayor que 1: {valoracion}", nameof(valoracion));
         }
     }
 }
--- tests/Solver.Tests/AtomoTests.cs
+++ tests/Solver.Tests/AtomoTests.cs
@@ -7,8 +7,24 @@
         [InlineData(-1)]
         public void Constructor_Posicion_DebeSerPositiva(int posicion)
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Atomo(posicion));
-            Assert.StartsWith("La posición debe ser positiva", ex.Message);
+            var ex = Assert.Throws<ArgumentException>(() => new Atomo(posicion, 1));
+            Assert.StartsWith($"La posición debe ser positiva: {posicion}", ex.Message);
+        }
+
+        [Fact]
+        public void Constructor_Valoracion_NoPuedeSerNegativa()
+        {
+            decimal valoracion = -0.1m;
+            var ex = Assert.Throws<ArgumentException>(() => new Atomo(1, valoracion));
+            Assert.StartsWith($"La valoración no puede ser negativa: {valoracion}", ex.Message);
+        }
+
+        [Fact]
+        public void Constructor_Valoracion_NoPuedeSerMayorQueUno()
+        {
+            decimal valoracion = 1.1m;
+            var ex = Assert.Throws<ArgumentException>(() => new Atomo(1, valoracion));
+            Assert.StartsWith($"La valoración no puede ser mayor que 1: {valoracion}", ex.Message);
         }
     }
 }
