425d4a59cd376e0dfc9c9403f189ec80120ab9df
DerivadaDX <derivadadx@gmail.com>
2025-03-17 23:22:32 -0300

    Se implementó `AgregarJugador` en `InstanciaProblema`.

--- src/Solver/InstanciaProblema.cs
+++ src/Solver/InstanciaProblema.cs
@@ -1,7 +1,34 @@
-﻿namespace Solver
+﻿
+namespace Solver
 {
     internal class InstanciaProblema
     {
+        internal InstanciaProblema()
+        {
+        }
+
         internal List<Jugador> Jugadores { get; } = [];
+        private HashSet<int> AtomosValorados { get; } = [];
+
+        internal void AgregarJugador(Jugador jugador)
+        {
+            if (Jugadores.Any(j => j.Id == jugador.Id))
+                throw new InvalidOperationException($"Ya existe un jugador con el id {jugador.Id}");
+
+            foreach (Atomo atomo in jugador.Valoraciones)
+            {
+                if (AtomosValorados.Contains(atomo.Posicion))
+                {
+                    string mensaje = $"El jugador #{jugador.Id} valora al átomo #{atomo.Posicion} que ya fue valorado por otro";
+                    throw new InvalidOperationException(mensaje);
+                }
+            }
+
+            Jugadores.Add(jugador);
+            foreach (Atomo atomo in jugador.Valoraciones)
+            {
+                AtomosValorados.Add(atomo.Posicion);
+            }
+        }
     }
 }
--- tests/Solver.Tests/InstanciaProblemaTests.cs
+++ tests/Solver.Tests/InstanciaProblemaTests.cs
@@ -5,9 +5,53 @@
         [Fact]
         public void Constructor_Jugadores_SeInicializaVacio()
         {
-            var instancia = new InstanciaProblema();
-            Assert.NotNull(instancia.Jugadores);
-            Assert.Empty(instancia.Jugadores);
+            var instanciaProblema = new InstanciaProblema();
+            Assert.NotNull(instanciaProblema.Jugadores);
+            Assert.Empty(instanciaProblema.Jugadores);
+        }
+
+        [Fact]
+        public void AgregarJugador_JugadorRepetido_LanzaExcepcion()
+        {
+            var jugador1 = new Jugador(1);
+            var jugador2 = new Jugador(1);
+
+            var instanciaProblema = new InstanciaProblema();
+            instanciaProblema.AgregarJugador(jugador1);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => instanciaProblema.AgregarJugador(jugador2));
+            Assert.Equal("Ya existe un jugador con el id 1", ex.Message);
+        }
+
+        [Fact]
+        public void AgregarJugador_JugadorValoraUnAtomoQueYaFueValorado_LanzaExcepcion()
+        {
+            var atomo = new Atomo(1, 1);
+            var jugador1 = new Jugador(1);
+            var jugador2 = new Jugador(2);
+            jugador1.AgregarValoracion(atomo);
+            jugador2.AgregarValoracion(atomo);
+
+            var instanciaProblema = new InstanciaProblema();
+            instanciaProblema.AgregarJugador(jugador1);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => instanciaProblema.AgregarJugador(jugador2));
+            Assert.Equal($"El jugador #{jugador2.Id} valora al átomo #{atomo.Posicion} que ya fue valorado por otro", ex.Message);
+        }
+
+        [Fact]
+        public void AgregarJugador_Jugador_SeAgregaALista()
+        {
+            var jugador1 = new Jugador(1);
+            var jugador2 = new Jugador(2);
+
+            var instanciaProblema = new InstanciaProblema();
+            instanciaProblema.AgregarJugador(jugador1);
+            instanciaProblema.AgregarJugador(jugador2);
+
+            Assert.True(instanciaProblema.Jugadores.Count == 2);
+            Assert.Same(jugador1, instanciaProblema.Jugadores[0]);
+            Assert.Same(jugador2, instanciaProblema.Jugadores[1]);
         }
     }
 }
