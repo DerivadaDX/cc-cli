f5b0f884e63aa22e80c881f31e311edafbfc42d4
DerivadaDX <derivadadx@gmail.com>
2025-03-17 22:44:36 -0300

    Se creó clase `Jugador` y su clase de tests.

---
+++ src/Solver/Jugador.cs
@@ -0,0 +1,12 @@
+﻿namespace Solver
+{
+    internal class Jugador
+    {
+        internal Jugador(int id)
+        {
+            Id = id;
+        }
+
+        internal int Id { get; }
+    }
+}
---
+++ tests/Solver.Tests/JugadorTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Solver.Tests
+{
+    public class JugadorTests
+    {
+        [Fact]
+        public void Constructor_Id_SeAsigna()
+        {
+            int id = 1;
+            var jugador = new Jugador(id);
+            Assert.Equal(id, jugador.Id);
+        }
+    }
+}
