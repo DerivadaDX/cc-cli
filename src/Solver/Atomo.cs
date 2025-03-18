d175d43c9b7c505b3d945faa931a2201fe54cb5d
DerivadaDX <derivadadx@gmail.com>
2025-03-17 21:59:35 -0300

    Se creó clase `Atomo` y un test para validar su posición.

---
+++ src/Solver/Atomo.cs
@@ -0,0 +1,11 @@
+﻿namespace Solver
+{
+    internal class Atomo
+    {
+        public Atomo(int posicion)
+        {
+            if (posicion <= 0)
+                throw new ArgumentException("La posición debe ser positiva", nameof(posicion));
+        }
+    }
+}
---
+++ tests/Solver.Tests/AtomoTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Solver.Tests
+{
+    public class AtomoTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Constructor_Posicion_DebeSerPositiva(int posicion)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new Atomo(posicion));
+            Assert.StartsWith("La posición debe ser positiva", ex.Message);
+        }
+    }
+}
