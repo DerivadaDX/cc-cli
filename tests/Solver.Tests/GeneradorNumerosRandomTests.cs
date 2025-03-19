d4c1bbaa4c0400854ff0e4755e7827c07ea8d69d
DerivadaDX <derivadadx@gmail.com>
2025-03-19 20:42:31 -0300

    feat(solver): implementación de generador de números aleatorios (#3)
    Este pull request introduce una nueva clase de generador de números aleatorios y su interfaz, junto con sus tests correspondientes. Los cambios principales incluyen la implementación de la clase `GeneradorNumerosRandom`, la interfaz `IGeneradorNumerosRandom` y la clase de tests `GeneradorNumerosRandomTests`.

---
+++ tests/Solver.Tests/GeneradorNumerosRandomTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Solver.Tests
+{
+    public class GeneradorNumerosRandomTests
+    {
+        [Fact]
+        public void GenerarNumerosRandom_SemillaNegativa_LanzaExcepcion()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new GeneradorNumerosRandom(-1));
+            Assert.StartsWith("La semilla no puede ser negativa: -1", ex.Message);
+        }
+
+        [Fact]
+        public void Siguiente_ConSemillaFija_DevuelveMismosNumeros()
+        {
+            var generador1 = new GeneradorNumerosRandom(123);
+            var generador2 = new GeneradorNumerosRandom(123);
+
+            int numero1 = generador1.Siguiente();
+            int numero2 = generador2.Siguiente();
+
+            Assert.Equal(numero1, numero2);
+        }
+
+        [Fact]
+        public void Siguiente_ConSemillaDiferente_DevuelveNumerosDiferentes()
+        {
+            var generador1 = new GeneradorNumerosRandom(123);
+            var generador2 = new GeneradorNumerosRandom(456);
+
+            int numero1 = generador1.Siguiente();
+            int numero2 = generador2.Siguiente();
+
+            Assert.NotEqual(numero1, numero2);
+        }
+
+        [Fact]
+        public void Siguiente_ConRango_DevuelveNumeroEnRango()
+        {
+            var generador = new GeneradorNumerosRandom();
+            int numero = generador.Siguiente(1, 100);
+            Assert.InRange(numero, 1, 99);
+        }
+    }
+}
