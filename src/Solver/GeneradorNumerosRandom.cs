8d710d699f26892d3c827440e702d9b1eae265ba
DerivadaDX <derivadadx@gmail.com>
2025-03-19 20:42:31 -0300

    feat(solver): implementación de generador de números aleatorios (#3)
    Este pull request introduce una nueva clase de generador de números aleatorios y su interfaz, junto con sus tests correspondientes. Los cambios principales incluyen la implementación de la clase `GeneradorNumerosRandom`, la interfaz `IGeneradorNumerosRandom` y la clase de tests `GeneradorNumerosRandomTests`.

---
+++ src/Solver/GeneradorNumerosRandom.cs
@@ -0,0 +1,30 @@
+﻿namespace Solver
+{
+    internal class GeneradorNumerosRandom : IGeneradorNumerosRandom
+    {
+        private readonly Random _random;
+
+        internal GeneradorNumerosRandom()
+        {
+            _random = new Random();
+        }
+
+        internal GeneradorNumerosRandom(int seed)
+        {
+            if (seed < 0)
+                throw new ArgumentException($"La semilla no puede ser negativa: {seed}", nameof(seed));
+
+            _random = new Random(seed);
+        }
+
+        public int Siguiente()
+        {
+            return _random.Next();
+        }
+
+        public int Siguiente(int minimo, int maximo)
+        {
+            return _random.Next(minimo, maximo);
+        }
+    }
+}
