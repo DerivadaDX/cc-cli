c135fcb2e2e5eaf459e58eeae064c03edf575c12
DerivadaDX <derivadadx@gmail.com>
2025-03-19 21:41:08 -0300

    Se creó clase abstracta `Individuo`.

---
+++ src/Solver/Individuos/Individuo.cs
@@ -0,0 +1,11 @@
+﻿using Solver.Fitness;
+
+namespace Solver.Individuos
+{
+    internal abstract class Individuo
+    {
+        internal abstract void Mutar();
+        internal abstract Individuo Cruzar(Individuo otro);
+        internal abstract void CalcularFitness(ICalculadoraFitness calculadoraFitness);
+    }
+}
