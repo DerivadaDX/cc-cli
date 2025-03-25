94e789ef4ba0038ba0cbcbf4491403f7811a6b1c
DerivadaDX <derivadadx@gmail.com>
2025-03-25 09:34:48 -0300

    feat(instancia-builder): implementación de builder para instancias de problemas (#9)
    Este pull request incluye la incorporación de un nuevo proyecto de consola `GeneradorInstancias` para generar instancias del problema de cake-cutting. También se creó el proyecto `Common` donde se movió la clase para generación de números random, esta librería de clases contendrá las que se usen en más de un proyecto.

--- src/Common/GeneradorNumerosRandomFactory.cs
+++ src/Common/GeneradorNumerosRandomFactory.cs
@@ -1,16 +1,16 @@
-﻿namespace Solver.Random
+﻿namespace Common
 {
-    internal class GeneradorNumerosRandomFactory
+    public class GeneradorNumerosRandomFactory
     {
         private static GeneradorNumerosRandom _generador;
 
-        internal static GeneradorNumerosRandom Crear()
+        public static GeneradorNumerosRandom Crear()
         {
             var generador = _generador ?? new GeneradorNumerosRandom();
             return generador;
         }
 
-        internal static GeneradorNumerosRandom Crear(int seed)
+        public static GeneradorNumerosRandom Crear(int seed)
         {
             var generador = _generador ?? new GeneradorNumerosRandom(seed);
             return generador;
@@ -20,7 +20,7 @@
         /// <summary>
         /// Solo usar para tests. Solamente está disponible en modo DEBUG.
         /// </summary>
-        internal static void SetearGenerador(GeneradorNumerosRandom generador)
+        public static void SetearGenerador(GeneradorNumerosRandom generador)
         {
             _generador = generador;
         }
---
+++ src/GeneradorInstancias/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GeneradorInstancias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
