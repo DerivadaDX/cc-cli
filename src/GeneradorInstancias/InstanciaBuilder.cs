73649e3e2a8da6cf7c49259a70ae715770e6544c
DerivadaDX <derivadadx@gmail.com>
2025-03-24 23:00:56 -0300

    Se implementó la creación de instancias con valoraciones disjuntas y no disjuntas.

--- src/GeneradorInstancias/InstanciaBuilder.cs
+++ src/GeneradorInstancias/InstanciaBuilder.cs
@@ -5,9 +5,11 @@ namespace GeneradorInstancias
 {
     internal class InstanciaBuilder
     {
+        private int _valorMaximo = 1000;
+        private bool _valoracionesDisjuntas = false;
+
         private int _cantidadAtomos;
         private int _cantidadJugadores;
-        private int _valorMaximo = 1000;
 
         private readonly GeneradorNumerosRandom _generadorNumerosRandom;
 
@@ -49,7 +51,82 @@ namespace GeneradorInstancias
             return this;
         }
 
+        internal InstanciaBuilder ConValoracionesDisjuntas(bool valoracionesDisjuntas)
+        {
+            _valoracionesDisjuntas = valoracionesDisjuntas;
+            return this;
+        }
+
         internal decimal[][] Build()
+        {
+            decimal[][] instancia;
+
+            if (_valoracionesDisjuntas)
+                instancia = ConstruirInstanciaDisjunta();
+            else
+                instancia = ConstruirInstanciaNoDisjunta();
+
+            return instancia;
+        }
+
+        private decimal[][] ConstruirInstanciaNoDisjunta()
+        {
+            decimal[][] instancia = ConstruirInstanciaVacia();
+
+            for (int indiceAtomo = 0; indiceAtomo < _cantidadAtomos; indiceAtomo++)
+            {
+                for (int indiceJugador = 0; indiceJugador < _cantidadJugadores; indiceJugador++)
+                {
+                    int valorAleatorio = _generadorNumerosRandom.Siguiente(1, _valorMaximo + 1);
+                    instancia[indiceAtomo][indiceJugador] = valorAleatorio;
+                }
+            }
+
+            return instancia;
+        }
+
+        private decimal[][] ConstruirInstanciaDisjunta()
+        {
+            if (_cantidadAtomos < _cantidadJugadores)
+            {
+                string mensaje = "No se puede generar una instancia con más jugadores que átomos si las valoraciones son disjuntas";
+                throw new InvalidOperationException(mensaje);
+            }
+
+            decimal[][] instancia = ConstruirInstanciaVacia();
+
+            List<int> atomosDisponibles = Enumerable.Range(0, _cantidadAtomos).ToList();
+            List<int> jugadoresDisponibles = Enumerable.Range(0, _cantidadJugadores).ToList();
+
+            while (jugadoresDisponibles.Count > 0)
+            {
+                int indiceAtomo = _generadorNumerosRandom.Siguiente(0, atomosDisponibles.Count);
+                int atomoElegido = atomosDisponibles[indiceAtomo];
+
+                int indiceJugador = _generadorNumerosRandom.Siguiente(0, jugadoresDisponibles.Count);
+                int jugadorElegido = jugadoresDisponibles[indiceJugador];
+
+                instancia[atomoElegido][jugadorElegido] = _generadorNumerosRandom.Siguiente(1, _valorMaximo + 1);
+
+                atomosDisponibles.RemoveAt(indiceAtomo);
+                jugadoresDisponibles.RemoveAt(indiceJugador);
+            }
+
+            while (atomosDisponibles.Count > 0)
+            {
+                int indiceAtomo = _generadorNumerosRandom.Siguiente(0, atomosDisponibles.Count);
+                int jugadorElegido = _generadorNumerosRandom.Siguiente(0, _cantidadJugadores);
+                int atomoElegido = atomosDisponibles[indiceAtomo];
+
+                instancia[atomoElegido][jugadorElegido] = _generadorNumerosRandom.Siguiente(1, _valorMaximo + 1);
+
+                atomosDisponibles.RemoveAt(indiceAtomo);
+            }
+
+            return instancia;
+        }
+
+        private decimal[][] ConstruirInstanciaVacia()
         {
             if (_cantidadAtomos == 0 || _cantidadJugadores == 0)
                 throw new InvalidOperationException("Debe especificar el número de átomos y jugadores antes de construir la instancia");
@@ -59,14 +136,6 @@ namespace GeneradorInstancias
             for (int indiceAtomo = 0; indiceAtomo < _cantidadAtomos; indiceAtomo++)
             {
                 instancia[indiceAtomo] = new decimal[_cantidadJugadores];
-            }
-
-            for (int indiceAtomo = 0; indiceAtomo < _cantidadAtomos; indiceAtomo++)
-            {
-                for (int indiceJugador = 0; indiceJugador < _cantidadJugadores; indiceJugador++)
-                {
-                    instancia[indiceAtomo][indiceJugador] = _generadorNumerosRandom.Siguiente(0, _valorMaximo + 1);
-                }
             }
 
             return instancia;
--- tests/GeneradorInstancias.Tests/InstanciaBuilderTests.cs
+++ tests/GeneradorInstancias.Tests/InstanciaBuilderTests.cs
@@ -57,6 +57,17 @@ namespace GeneradorInstancias.Tests
         }
 
         [Fact]
+        public void Build_ValoracionesDisjuntasConMasJugadoresQueAtomos_LanzaExcepcion()
+        {
+            var builder = new InstanciaBuilder()
+                .ConCantidadDeAtomos(2)
+                .ConCantidadDeJugadores(3)
+                .ConValoracionesDisjuntas(true);
+            var ex = Assert.Throws<InvalidOperationException>(builder.Build);
+            Assert.Equal("No se puede generar una instancia con más jugadores que átomos si las valoraciones son disjuntas", ex.Message);
+        }
+
+        [Fact]
         public void Build_AtomosYJugadoresValidos_DevuelveMatrizCorrecta()
         {
             var builder = new InstanciaBuilder()
@@ -69,7 +80,6 @@ namespace GeneradorInstancias.Tests
             Assert.Equal(2, instancia[0].Length);
         }
 
-
         [Theory]
         [InlineData(1)]
         [InlineData(50)]
@@ -80,17 +90,56 @@ namespace GeneradorInstancias.Tests
                 .ConCantidadDeJugadores(3)
                 .ConValorMaximo(valorMaximo);
 
-            decimal[][] matriz = builder.Build();
+            decimal[][] instancia = builder.Build();
 
-            Assert.InRange(matriz[0][0], 0, valorMaximo);
-            Assert.InRange(matriz[0][1], 0, valorMaximo);
-            Assert.InRange(matriz[0][2], 0, valorMaximo);
-            Assert.InRange(matriz[1][0], 0, valorMaximo);
-            Assert.InRange(matriz[1][1], 0, valorMaximo);
-            Assert.InRange(matriz[1][2], 0, valorMaximo);
-            Assert.InRange(matriz[2][0], 0, valorMaximo);
-            Assert.InRange(matriz[2][1], 0, valorMaximo);
-            Assert.InRange(matriz[2][2], 0, valorMaximo);
+            Assert.InRange(instancia[0][0], 0, valorMaximo);
+            Assert.InRange(instancia[0][1], 0, valorMaximo);
+            Assert.InRange(instancia[0][2], 0, valorMaximo);
+            Assert.InRange(instancia[1][0], 0, valorMaximo);
+            Assert.InRange(instancia[1][1], 0, valorMaximo);
+            Assert.InRange(instancia[1][2], 0, valorMaximo);
+            Assert.InRange(instancia[2][0], 0, valorMaximo);
+            Assert.InRange(instancia[2][1], 0, valorMaximo);
+            Assert.InRange(instancia[2][2], 0, valorMaximo);
+        }
+
+        [Fact]
+        public void Build_ValoracionesNoDisjuntas_NingunaCeldaValeCero()
+        {
+            var builder = new InstanciaBuilder()
+                .ConCantidadDeAtomos(3)
+                .ConCantidadDeJugadores(3)
+                .ConValorMaximo(1)
+                .ConValoracionesDisjuntas(false);
+
+            decimal[][] instancia = builder.Build();
+
+            Assert.NotEqual(0, instancia[0][0]);
+            Assert.NotEqual(0, instancia[0][1]);
+            Assert.NotEqual(0, instancia[0][2]);
+            Assert.NotEqual(0, instancia[1][0]);
+            Assert.NotEqual(0, instancia[1][1]);
+            Assert.NotEqual(0, instancia[1][2]);
+            Assert.NotEqual(0, instancia[2][0]);
+            Assert.NotEqual(0, instancia[2][1]);
+            Assert.NotEqual(0, instancia[2][2]);
+        }
+
+        [Fact]
+        public void Build_ValoracionesDisjuntas_CadaFilaTieneUnSoloValorPositivo()
+        {
+            var builder = new InstanciaBuilder()
+                .ConCantidadDeAtomos(4)
+                .ConCantidadDeJugadores(3)
+                .ConValorMaximo(5)
+                .ConValoracionesDisjuntas(true);
+
+            decimal[][] instancia = builder.Build();
+
+            Assert.Equal(1, instancia[0].Count(v => v > 0));
+            Assert.Equal(1, instancia[1].Count(v => v > 0));
+            Assert.Equal(1, instancia[2].Count(v => v > 0));
+            Assert.Equal(1, instancia[3].Count(v => v > 0));
         }
     }
 }
