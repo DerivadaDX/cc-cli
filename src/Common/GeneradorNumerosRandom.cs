f5ec1656f13d2f1181496dd0e8a70bd5cb6d33f0
DerivadaDX <derivadadx@gmail.com>
2025-03-24 21:00:45 -0300

    Se creó proyecto Common y se movió el generador de números random ahí.

---
+++ src/Common/GeneradorNumerosRandom.cs
@@ -0,0 +1,30 @@
+﻿namespace Common
+{
+    public class GeneradorNumerosRandom
+    {
+        private readonly Random _random;
+
+        public GeneradorNumerosRandom()
+        {
+            _random = new Random();
+        }
+
+        public GeneradorNumerosRandom(int seed)
+        {
+            if (seed < 0)
+                throw new ArgumentException($"La semilla no puede ser negativa: {seed}", nameof(seed));
+
+            _random = new Random(seed);
+        }
+
+        public virtual int Siguiente()
+        {
+            return _random.Next();
+        }
+
+        public virtual int Siguiente(int minimo, int maximo)
+        {
+            return _random.Next(minimo, maximo);
+        }
+    }
+}
