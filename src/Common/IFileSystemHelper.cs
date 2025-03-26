b797fb24460bab89393f61d9f71cb32edbac34bc
DerivadaDX <derivadadx@gmail.com>
2025-03-26 09:09:22 -0300

    Se implementó la clase `EscritorInstancia` junto con sus tests. Se creó la interface `IFileSystemHelper` y se implementó en `FileSystemHelper`.

---
+++ src/Common/IFileSystemHelper.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Common
+{
+    public interface IFileSystemHelper
+    {
+        void CreateDirectory(string path);
+        bool DirectoryExists(string path);
+        void WriteAllLines(string path, IEnumerable<string> lines);
+    }
+}
