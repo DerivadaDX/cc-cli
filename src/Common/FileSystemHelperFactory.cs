ccb4595f9d46f52211ce8441f76b38648c94f453
DerivadaDX <derivadadx@gmail.com>
2025-03-28 08:21:25 -0300

    Se implementó factory de `FileSystemHelper`.

---
+++ src/Common/FileSystemHelperFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace Common
+{
+    public class FileSystemHelperFactory
+    {
+        private static FileSystemHelper _helper;
+
+        public static FileSystemHelper Crear()
+        {
+            var helper = _helper ?? new FileSystemHelper();
+            return helper;
+        }
+
+#if DEBUG
+        /// <summary>
+        /// Solo usar para tests. Solamente está disponible en modo DEBUG.
+        /// </summary>
+        public static void SetearHelper(FileSystemHelper helper)
+        {
+            _helper = helper;
+        }
+#endif
+    }
+}
---
+++ tests/Common.Tests/FileSystemHelperFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Common.Tests
+{
+    public class FileSystemHelperFactoryTests : IDisposable
+    {
+        public void Dispose()
+        {
+            FileSystemHelperFactory.SetearHelper(null);
+            GC.SuppressFinalize(this);
+        }
+
+        [Fact]
+        public void Crear_InstanciaDevuelta_EsValida()
+        {
+            var helper = FileSystemHelperFactory.Crear();
+            Assert.NotNull(helper);
+            Assert.IsType<FileSystemHelper>(helper);
+        }
+
+        [Fact]
+        public void SetearHelper_Helper_SeSeteaCorrectamente()
+        {
+            var helperMock = new FileSystemHelper();
+            FileSystemHelperFactory.SetearHelper(helperMock);
+
+            var helper = FileSystemHelperFactory.Crear();
+
+            Assert.Same(helperMock, helper);
+        }
+    }
+}
