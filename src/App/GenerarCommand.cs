6720f1e206205365001fd7ad7c6d8c9b35071de5
DerivadaDX <derivadadx@gmail.com>
2025-03-28 08:55:01 -0300

    Se implementó `GenerarCommand`, la clase para el comando generar.

---
+++ src/App/GenerarCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.CommandLine;
+using Common;
+using GeneradorInstancia;
+
+namespace App
+{
+    internal static class GenerarCommand
+    {
+        internal static Command Create()
+        {
+            var command = new Command("generar", "Genera una nueva instancia");
+
+            var atomosOption = new Option<int>("--atomos") { IsRequired = true };
+            var agentesOption = new Option<int>("--agentes") { IsRequired = true };
+            var valorMaximoOption = new Option<int>("--valor-maximo", () => 1000);
+            var outputOption = new Option<string>("--output", () => "instancia.dat");
+            var disjuntasOption = new Option<bool>("--disjuntas");
+
+            command.AddOption(atomosOption);
+            command.AddOption(agentesOption);
+            command.AddOption(valorMaximoOption);
+            command.AddOption(outputOption);
+            command.AddOption(disjuntasOption);
+
+            command.SetHandler((atomos, agentes, max, output, disjuntas) =>
+            {
+                var parametros = new ParametrosGeneracion(atomos, agentes, max, output, disjuntas);
+                var builder = new InstanciaBuilder();
+                var escritor = new EscritorInstancia(new FileSystemHelper());
+                Handler(parametros, builder, escritor);
+            }, atomosOption, agentesOption, valorMaximoOption, outputOption, disjuntasOption);
+
+            return command;
+        }
+
+        internal static void Handler(ParametrosGeneracion parametros, InstanciaBuilder builder, EscritorInstancia escritor)
+        {
+            builder
+                .ConCantidadDeAtomos(parametros.Atomos)
+                .ConCantidadDeJugadores(parametros.Agentes)
+                .ConValorMaximo(parametros.ValorMaximo)
+                .ConValoracionesDisjuntas(parametros.ValoracionesDisjuntas);
+
+            escritor.EscribirInstancia(builder.Build(), parametros.RutaSalida);
+        }
+    }
+}
---
+++ tests/App.Tests/GenerarCommandTests.cs
@@ -0,0 +1,74 @@
+using System.CommandLine;
+using System.CommandLine.Parsing;
+using Common;
+using GeneradorInstancia;
+using NSubstitute;
+
+namespace App.Tests
+{
+    public class GenerarCommandTests
+    {
+        [Fact]
+        public void Create_ValorMaximoNoEspecificado_UsaValorPorDefecto()
+        {
+            var command = GenerarCommand.Create();
+            var valorMaximoOption = (Option<int>)command.Options.First(o => o.Name == "valor-maximo");
+            int valorMaximo = command.Parse("generar --atomos 5 --agentes 3").GetValueForOption(valorMaximoOption);
+
+            Assert.Equal(1000, valorMaximo);
+        }
+        [Fact]
+        public void Create_OutputNoEspecificada_UsaValorPorDefecto()
+        {
+            var command = GenerarCommand.Create();
+            var outputOption = (Option<string>)command.Options.First(o => o.Name == "output");
+            string output = command.Parse("generar --atomos 5 --agentes 3").GetValueForOption(outputOption);
+
+            Assert.Equal("instancia.dat", output);
+        }
+
+        [Fact]
+        public void Create_Command_ConfiguraOpcionesCorrectamente()
+        {
+            var command = GenerarCommand.Create();
+
+            Assert.Equal("generar", command.Name);
+            Assert.Contains(command.Options, o => o.Name == "atomos");
+            Assert.Contains(command.Options, o => o.Name == "agentes");
+            Assert.Contains(command.Options, o => o.Name == "valor-maximo");
+            Assert.Contains(command.Options, o => o.Name == "output");
+            Assert.Contains(command.Options, o => o.Name == "disjuntas");
+        }
+
+        [Fact]
+        public void Handler_ConParametrosValidos_GeneraYEscribeInstancia()
+        {
+            var instanciaMock = new decimal[1, 1];
+
+            var builder = Substitute.For<InstanciaBuilder>();
+            builder.ConCantidadDeAtomos(Arg.Any<int>()).Returns(builder);
+            builder.ConCantidadDeJugadores(Arg.Any<int>()).Returns(builder);
+            builder.ConValorMaximo(Arg.Any<int>()).Returns(builder);
+            builder.ConValoracionesDisjuntas(Arg.Any<bool>()).Returns(builder);
+            builder.Build().Returns(instanciaMock);
+
+            var escritor = Substitute.For<EscritorInstancia>(Substitute.For<FileSystemHelper>());
+
+            var parametros = new ParametrosGeneracion(
+                atomos: 5,
+                agentes: 3,
+                valorMaximo: 100,
+                rutaSalida: "instancia.dat",
+                valoracionesDisjuntas: true);
+
+            GenerarCommand.Handler(parametros, builder, escritor);
+
+            builder.Received(1).ConCantidadDeAtomos(5);
+            builder.Received(1).ConCantidadDeJugadores(3);
+            builder.Received(1).ConValorMaximo(100);
+            builder.Received(1).ConValoracionesDisjuntas(true);
+            builder.Received(1).Build();
+            escritor.Received(1).EscribirInstancia(instanciaMock, "instancia.dat");
+        }
+    }
+}
