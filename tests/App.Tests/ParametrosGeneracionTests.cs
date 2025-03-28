39d899189d6eb56b6f2d121d773b982d07568d31
DerivadaDX <derivadadx@gmail.com>
2025-03-28 08:28:48 -0300

    Obsessive commit.

--- tests/App.Tests/ParametrosGeneracionTests.cs
+++ tests/App.Tests/ParametrosGeneracionTests.cs
@@ -15,14 +15,14 @@ namespace App.Tests
         [Fact]
         public void Constructor_AtomosInvalido_LanzaArgumentException()
         {
-            var ex = Assert.Throws<ArgumentException>(() => new ParametrosGeneracion(0, 1, 100, "ruta.txt", false));
+            var ex = Assert.Throws<ArgumentException>(() => new ParametrosGeneracion(0, 1, 100, "instancia.dat", false));
             Assert.StartsWith("Se requieren al menos 1 átomo", ex.Message);
         }
 
         [Fact]
         public void Constructor_AgentesInvalido_LanzaArgumentException()
         {
-            var ex = Assert.Throws<ArgumentException>(() => new ParametrosGeneracion(1, 0, 100, "ruta.txt", false));
+            var ex = Assert.Throws<ArgumentException>(() => new ParametrosGeneracion(1, 0, 100, "instancia.dat", false));
             Assert.StartsWith("Se requieren al menos 1 agente", ex.Message);
         }
 
@@ -31,7 +31,7 @@ namespace App.Tests
         [InlineData(-1)]
         public void Constructor_ValorMaximoInvalido_LanzaArgumentException(int valorMaximo)
         {
-            var ex = Assert.Throws<ArgumentException>(() => new ParametrosGeneracion(1, 1, valorMaximo, "ruta.txt", false));
+            var ex = Assert.Throws<ArgumentException>(() => new ParametrosGeneracion(1, 1, valorMaximo, "instancia.dat", false));
             Assert.StartsWith("El valor máximo debe ser positivo", ex.Message);
         }
 
@@ -59,12 +59,12 @@ namespace App.Tests
         [Fact]
         public void Constructor_DatosValidos_CreaInstanciaCorrectamente()
         {
-            var parametros = new ParametrosGeneracion(5, 3, 100, "test.txt", true);
+            var parametros = new ParametrosGeneracion(5, 3, 100, "instancia.dat", true);
 
             Assert.Equal(5, parametros.Atomos);
             Assert.Equal(3, parametros.Agentes);
             Assert.Equal(100, parametros.ValorMaximo);
-            Assert.Equal("test.txt", parametros.RutaSalida);
+            Assert.Equal("instancia.dat", parametros.RutaSalida);
             Assert.True(parametros.ValoracionesDisjuntas);
         }
     }
