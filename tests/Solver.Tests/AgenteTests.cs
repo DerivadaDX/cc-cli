b00b5e638f872639381b63b21045dbad7eaa5665
DerivadaDX <derivadadx@gmail.com>
2025-04-14 20:40:07 -0300

    Extracción de instancia bajo prueba a variable de instancia en `AgenteTests`.

--- tests/Solver.Tests/AgenteTests.cs
+++ tests/Solver.Tests/AgenteTests.cs
@@ -2,20 +2,26 @@
 {
     public class AgenteTests
     {
+        private const int IdAgente = 1;
+
+        private readonly Agente _agente;
+
+        public AgenteTests()
+        {
+            _agente = new Agente(IdAgente);
+        }
+
         [Fact]
         public void Constructor_Id_SeAsigna()
         {
-            int id = 1;
-            var agente = new Agente(id);
-            Assert.Equal(id, agente.Id);
+            Assert.Equal(IdAgente, _agente.Id);
         }
 
         [Fact]
         public void Constructor_Valoraciones_SeInicializaVacia()
         {
-            var agente = new Agente(1);
-            Assert.NotNull(agente.Valoraciones);
-            Assert.Empty(agente.Valoraciones);
+            Assert.NotNull(_agente.Valoraciones);
+            Assert.Empty(_agente.Valoraciones);
         }
 
         [Fact]
@@ -24,24 +30,22 @@
             var atomo1 = new Atomo(1, 1);
             var atomo2 = new Atomo(2, 1);
 
-            var agente = new Agente(1);
-            agente.AgregarValoracion(atomo1);
-            agente.AgregarValoracion(atomo2);
+            _agente.AgregarValoracion(atomo1);
+            _agente.AgregarValoracion(atomo2);
 
-            Assert.True(agente.Valoraciones.Count == 2);
-            Assert.Same(atomo1, agente.Valoraciones[0]);
-            Assert.Same(atomo2, agente.Valoraciones[1]);
+            Assert.True(_agente.Valoraciones.Count == 2);
+            Assert.Same(atomo1, _agente.Valoraciones[0]);
+            Assert.Same(atomo2, _agente.Valoraciones[1]);
         }
 
         [Fact]
         public void AgregarValoracion_ValoracionSobreMismoAtomo_LanzaInvalidOperationException()
         {
-            int posicionAtomo = 1;
+            const int posicionAtomo = 1;
 
-            var agente = new Agente(1);
-            agente.AgregarValoracion(new Atomo(posicionAtomo, 1));
+            _agente.AgregarValoracion(new Atomo(posicionAtomo, 1));
 
-            var ex = Assert.Throws<InvalidOperationException>(() => agente.AgregarValoracion(new Atomo(posicionAtomo, 0)));
+            var ex = Assert.Throws<InvalidOperationException>(() => _agente.AgregarValoracion(new Atomo(posicionAtomo, 0)));
             Assert.Equal($"Ya existe una valoración para el átomo #{posicionAtomo}", ex.Message);
         }
     }
