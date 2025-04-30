c287f55af6633df72e04d4cf2897061d5adbeb7d
DerivadaDX <derivadadx@gmail.com>
2025-04-30 14:54:37 -0300

    Creación de clase `LectorInstancia` y tests de su constructor.

---
+++ src/Solver/LectorInstancia.cs
@@ -0,0 +1,12 @@
+﻿using Common;
+
+namespace Solver
+{
+    internal class LectorInstancia
+    {
+        public LectorInstancia(FileSystemHelper fileSystemHelper)
+        {
+            ArgumentNullException.ThrowIfNull(fileSystemHelper, nameof(fileSystemHelper));
+        }
+    }
+}
---
+++ tests/Solver.Tests/LectorInstanciaTests.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using NSubstitute;
+
+namespace Solver.Tests
+{
+    public class LectorInstanciaTests
+    {
+        [Fact]
+        public void Constructor_FileSystemHelperNull_ArrojaArgumentNullException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new LectorInstancia(null));
+            Assert.Contains("fileSystemHelper", ex.Message);
+        }
+    }
+}
