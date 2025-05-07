5647b2a9430f50759abac07f2342ded42046d44f
DerivadaDX <derivadadx@gmail.com>
2025-05-06 22:21:35 -0300

    Se creó la clase `AlgoritmoGenetico`, se definió su ctor y se valida su parámetro.

---
+++ src/Solver/AlgoritmoGenetico.cs
@@ -0,0 +1,15 @@
+﻿namespace Solver
+{
+    internal class AlgoritmoGenetico
+    {
+        private readonly int _tamañoPoblacion;
+
+        public AlgoritmoGenetico(int tamañoPoblacion)
+        {
+            if (tamañoPoblacion <= 0)
+                throw new ArgumentException($"El tamaño de la población debe ser positivo: {tamañoPoblacion}");
+
+            _tamañoPoblacion = tamañoPoblacion;
+        }
+    }
+}
---
+++ tests/Solver.Tests/AlgoritmoGeneticoTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Solver.Tests
+{
+    public class AlgoritmoGeneticoTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Constructor_TamañoPoblacionInvalido_ArrojaArgumentException(int tamañoPoblacion)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new AlgoritmoGenetico(tamañoPoblacion));
+            Assert.StartsWith($"El tamaño de la población debe ser positivo: {tamañoPoblacion}", ex.Message);
+        }
+    }
+}
