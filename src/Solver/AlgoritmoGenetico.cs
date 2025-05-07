fee599b15ba9ae8519a408dcba883d78d55603ab
DerivadaDX <derivadadx@gmail.com>
2025-05-07 09:10:21 -0300

    Se valida que la población recibida en el ctor de `AlgoritmoGenetico` no esté vacía.

--- src/Solver/AlgoritmoGenetico.cs
+++ src/Solver/AlgoritmoGenetico.cs
@@ -7,6 +7,8 @@ namespace Solver
         public AlgoritmoGenetico(List<Individuo> poblacion)
         {
             ArgumentNullException.ThrowIfNull(poblacion, nameof(poblacion));
+            if (poblacion.Count == 0)
+                throw new ArgumentException("La población no puede estar vacía", nameof(poblacion));
         }
     }
 }
--- tests/Solver.Tests/AlgoritmoGeneticoTests.cs
+++ tests/Solver.Tests/AlgoritmoGeneticoTests.cs
@@ -8,5 +8,12 @@
             var ex = Assert.Throws<ArgumentNullException>(() => new AlgoritmoGenetico(null));
             Assert.Equal("poblacion", ex.ParamName);
         }
+
+        [Fact]
+        public void Constructor_PoblacionVacia_ArrojaArgumentException()
+        {
+            var ex = Assert.Throws<ArgumentException>(() => new AlgoritmoGenetico([]));
+            Assert.StartsWith("La población no puede estar vacía", ex.Message);
+        }
     }
 }
