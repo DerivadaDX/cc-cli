50e605774ad2038dcea6c8250dd67e484224af55
DerivadaDX <derivadadx@gmail.com>
2025-05-07 10:09:49 -0300

    Se creó el método `Ejecutar` en `AlgoritmoGenetico` y se hace que devuelva el único individuo si la población es de uno solo.

--- src/Solver/AlgoritmoGenetico.cs
+++ src/Solver/AlgoritmoGenetico.cs
@@ -4,11 +4,23 @@ namespace Solver
 {
     internal class AlgoritmoGenetico
     {
+        private readonly List<Individuo> _poblacion;
+
         public AlgoritmoGenetico(List<Individuo> poblacion)
         {
             ArgumentNullException.ThrowIfNull(poblacion, nameof(poblacion));
             if (poblacion.Count == 0)
                 throw new ArgumentException("La población no puede estar vacía", nameof(poblacion));
+
+            _poblacion = poblacion;
+        }
+
+        public Individuo Ejecutar()
+        {
+            if (_poblacion.Count == 1)
+                return _poblacion[0];
+
+            return null;
         }
     }
 }
--- tests/Solver.Tests/AlgoritmoGeneticoTests.cs
+++ tests/Solver.Tests/AlgoritmoGeneticoTests.cs
@@ -1,4 +1,7 @@
-﻿namespace Solver.Tests
+﻿using NSubstitute;
+using Solver.Individuos;
+
+namespace Solver.Tests
 {
     public class AlgoritmoGeneticoTests
     {
@@ -14,6 +17,23 @@
         {
             var ex = Assert.Throws<ArgumentException>(() => new AlgoritmoGenetico([]));
             Assert.StartsWith("La población no puede estar vacía", ex.Message);
+        }
+
+        [Fact]
+        public void Ejecutar_PoblacionDeUnSoloIndividuo_DevuelveElMismoIndividuo()
+        {
+            var instanciaProblema = InstanciaProblema.CrearDesdeMatrizDeValoraciones(new decimal[,]
+            {
+                { 1m, 0m },
+                { 0m, 1m },
+                { 0m, 1m },
+            });
+            var individuo = Substitute.For<Individuo>(new List<int> { 1, 1, 2 }, instanciaProblema);
+
+            var algoritmoGenetico = new AlgoritmoGenetico([individuo]);
+            Individuo resultado = algoritmoGenetico.Ejecutar();
+
+            Assert.Same(individuo, resultado);
         }
     }
 }
