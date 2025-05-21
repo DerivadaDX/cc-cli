066054b5e9d19c1f581bd5535a1cdce19ee3a6fa
DerivadaDX <derivadadx@gmail.com>
2025-05-21 08:26:13 -0300

    Creación de `ResolverCommand` y sus tests.

---
+++ src/App/ResolverCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.CommandLine;
+using Common;
+using Solver;
+
+namespace App
+{
+    internal class ResolverCommand
+    {
+        internal static Command Create()
+        {
+            var command = new Command("resolver", "Resuelve una instancia");
+            var instanciaOption = new Option<string>("--instancia") { IsRequired = true };
+            var maxGeneracionesOption = new Option<int>("--max-generaciones", () => 0);
+
+            command.AddOption(instanciaOption);
+            command.AddOption(maxGeneracionesOption);
+
+            command.SetHandler((rutaInstancia, maxGeneraciones) =>
+            {
+                var fileSystemHelper = FileSystemHelperFactory.Crear();
+                var lectorMatrizValoraciones = new LectorArchivoMatrizValoraciones(fileSystemHelper);
+
+                Handler(lectorMatrizValoraciones, rutaInstancia, maxGeneraciones);
+            }, instanciaOption, maxGeneracionesOption);
+
+            return command;
+        }
+
+        internal static void Handler(LectorArchivoMatrizValoraciones lector, string rutaInstancia, int maxGeneraciones)
+        {
+            decimal[,] matrizValoraciones = lector.Leer(rutaInstancia);
+            var instanciaProblema = InstanciaProblema.CrearDesdeMatrizDeValoraciones(matrizValoraciones);
+
+            // TODO: crear población
+            // TODO: crear algoritmo genético
+            // TODO: ejecutar algoritmo genético
+        }
+    }
+}
---
+++ tests/App.Tests/ResolverCommandTests.cs
@@ -0,0 +1,31 @@
+﻿using System.CommandLine;
+
+namespace App.Tests
+{
+    public class ResolverCommandTests
+    {
+        private readonly Command _command;
+
+        public ResolverCommandTests()
+        {
+            _command = ResolverCommand.Create();
+        }
+
+        [Fact]
+        public void Create_NombreYOpciones_ConfiguradasCorrectamente()
+        {
+            Assert.Equal("resolver", _command.Name);
+            Assert.Contains(_command.Options, o => o.Name == "instancia");
+            Assert.Contains(_command.Options, o => o.Name == "max-generaciones");
+        }
+
+        [Fact]
+        public void Create_MaxGeneracionesNoEspecificado_UsaValorPorDefecto()
+        {
+            var maxGeneracionesOption = (Option<int>)_command.Options.First(o => o.Name == "max-generaciones");
+            int maxGeneraciones = _command.Parse("resolver --instancia instancia.dat").GetValueForOption(maxGeneracionesOption);
+
+            Assert.Equal(0, maxGeneraciones);
+        }
+    }
+}
