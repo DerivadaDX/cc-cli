8770639ffe0c15d58671d06a88733b9dd6accc59
DerivadaDX <derivadadx@gmail.com>
2025-06-05 23:13:34 -0300

    Corrección en test y refactor.

--- tests/Solver.Tests/Individuos/IndividuoIntercambioAsignacionesTests.cs
+++ tests/Solver.Tests/Individuos/IndividuoIntercambioAsignacionesTests.cs
@@ -17,7 +17,7 @@ public class IndividuoIntercambioAsignacionesTests : IDisposable
     {
         var random = Substitute.For<GeneradorNumerosRandom>();
         random.Siguiente(1, 3).Returns(1);
-        random.Siguiente(4).Returns(0);
+        random.Siguiente(3).Returns(0);
         random.Siguiente(1, 4).Returns(2);
         GeneradorNumerosRandomFactory.SetearGenerador(random);
 
@@ -28,12 +28,11 @@ public class IndividuoIntercambioAsignacionesTests : IDisposable
             { 0m, 0m, 1m, 0m },
             { 0m, 0m, 0m, 1m },
         });
+        var padre1 = new IndividuoIntercambioAsignaciones([1, 2, 3, 1, 2, 3, 4], problema, new CalculadoraFitness());
+        var padre2 = new IndividuoIntercambioAsignaciones([3, 3, 3, 2, 3, 4, 1], problema, new CalculadoraFitness());
 
-        var padre1 = new IndividuoStub([1, 2, 3, 1, 2, 3, 4], problema);
-        var padre2 = new IndividuoStub([3, 3, 3, 2, 3, 4, 1], problema);
+        Individuo hijo = padre1.Cruzar(padre2);
 
-        var hijo = (IndividuoStub)padre1.Cruzar(padre2);
-        
         Assert.Equal([1, 3, 3], hijo.Cromosoma.Take(3));
         Assert.Equal([1, 2, 3, 4], hijo.Cromosoma.Skip(3));
     }
@@ -43,7 +42,7 @@ public class IndividuoIntercambioAsignacionesTests : IDisposable
     {
         var random = Substitute.For<GeneradorNumerosRandom>();
         random.Siguiente(1, 3).Returns(2);
-        random.Siguiente(4).Returns(1);
+        random.Siguiente(3).Returns(1);
         random.Siguiente(2, 4).Returns(3);
         GeneradorNumerosRandomFactory.SetearGenerador(random);
 
@@ -54,11 +53,10 @@ public class IndividuoIntercambioAsignacionesTests : IDisposable
             { 0m, 0m, 1m, 0m },
             { 0m, 0m, 0m, 1m },
         });
+        var padre1 = new IndividuoIntercambioAsignaciones([1, 2, 3, 1, 2, 3, 4], problema, new CalculadoraFitness());
+        var padre2 = new IndividuoIntercambioAsignaciones([3, 4, 4, 4, 3, 2, 1], problema, new CalculadoraFitness());
 
-        var padre1 = new IndividuoStub([1, 2, 3, 1, 2, 3, 4], problema);
-        var padre2 = new IndividuoStub([3, 4, 4, 4, 3, 2, 1], problema);
-
-        var hijo = (IndividuoStub)padre1.Cruzar(padre2);
+        Individuo hijo = padre1.Cruzar(padre2);
 
         Assert.Equal([1, 2, 4], hijo.Cromosoma.Take(3));
         Assert.Equal([1, 2, 3, 4], hijo.Cromosoma.Skip(3));
@@ -68,33 +66,13 @@ public class IndividuoIntercambioAsignacionesTests : IDisposable
     public void Cruzar_CromosomaUnAgente_NoLanzaExcepcion()
     {
         var problema = InstanciaProblema.CrearDesdeMatrizDeValoraciones(new decimal[,] { { 1m } });
+        var padre1 = new IndividuoIntercambioAsignaciones([1], problema, new CalculadoraFitness());
+        var padre2 = new IndividuoIntercambioAsignaciones([1], problema, new CalculadoraFitness());
 
-        var padre1 = new IndividuoStub([1], problema);
-        var padre2 = new IndividuoStub([1], problema);
+        Individuo hijo = padre1.Cruzar(padre2);
 
-        var hijo = padre1.Cruzar(padre2);
-
-        var cromosomaHijo = ((IndividuoStub)hijo).Cromosoma;
-        Assert.Single(cromosomaHijo);
-        Assert.Equal(1, cromosomaHijo[0]);
-    }
-
-    private class IndividuoStub : IndividuoIntercambioAsignaciones
-    {
-        internal IndividuoStub(List<int> cromosoma, InstanciaProblema problema)
-            : base(cromosoma, problema, new CalculadoraFitness())
-        {
-        }
-
-        internal override Individuo Cruzar(Individuo otro)
-        {
-            var baseHijo = (IndividuoIntercambioAsignaciones)base.Cruzar(otro);
-            var field = typeof(Individuo).GetField("_cromosoma", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            var lista = (List<int>)field!.GetValue(baseHijo)!;
-            return new IndividuoStub(lista.ToList(), _problema);
-        }
-
-        internal List<int> Cromosoma => _cromosoma;
+        Assert.Single(hijo.Cromosoma);
+        Assert.Equal(1, hijo.Cromosoma[0]);
     }
 }
 
