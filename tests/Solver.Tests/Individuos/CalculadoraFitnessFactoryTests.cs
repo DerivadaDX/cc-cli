7145865d167cc999c8af37e25fd3d53f2536b7cb
DerivadaDX <derivadadx@gmail.com>
2025-06-08 14:32:50 -0300

    feat(individuos): implementación de clases para creación de `Individuo` aleatorio (#43)
    En este PR se definió la interfaz `IIndividuoFactory` para poder crear
    aleatoriamente distintos tipos de individuos y se implementó
    `IndividuoIntercambioAsignacionesFactory`. Se creó
    `CalculadoraFitnessFactory`.

---
+++ tests/Solver.Tests/Individuos/CalculadoraFitnessFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using Solver.Individuos;
+
+namespace Solver.Tests.Individuos
+{
+    public class CalculadoraFitnessFactoryTests : IDisposable
+    {
+        public void Dispose()
+        {
+            CalculadoraFitnessFactory.SetearCalculadora(null);
+            GC.SuppressFinalize(this);
+        }
+
+        [Fact]
+        public void Crear_InstanciaDevuelta_EsValida()
+        {
+            var calculadora = CalculadoraFitnessFactory.Crear();
+            Assert.NotNull(calculadora);
+            Assert.IsType<CalculadoraFitness>(calculadora);
+        }
+
+        [Fact]
+        public void SetearCalculadora_Calculadora_SeSeteaCorrectamente()
+        {
+            var calculadora1 = new CalculadoraFitness();
+            CalculadoraFitnessFactory.SetearCalculadora(calculadora1);
+
+            var calculadora2 = CalculadoraFitnessFactory.Crear();
+
+            Assert.Same(calculadora1, calculadora2);
+        }
+    }
+}
