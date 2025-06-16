9e42282cc4e5727ca57aeee3515329eea1ea6f44
DerivadaDX <derivadadx@gmail.com>
2025-06-16 19:32:11 -0300

    Revert "Cambios parciales."
    This reverts commit 6097e20d05723c7371320a1e755b7253ac664876.

--- src/App/Commands/Resolver/ResolverCommand.cs
+++ src/App/Commands/Resolver/ResolverCommand.cs
@@ -8,15 +8,7 @@ namespace App.Commands.Resolver
 {
     internal class ResolverCommand
     {
-        private readonly Presentador _presentador;
-
-        internal ResolverCommand(Presentador presentador)
-        {
-            ArgumentNullException.ThrowIfNull(presentador, nameof(presentador));
-            _presentador = presentador;
-        }
-
-        internal Command Crear()
+        internal static Command Create()
         {
             var command = new Command("resolver", "Resuelve una instancia");
             var instanciaOption = new Option<string>("--instancia")
@@ -36,19 +28,32 @@ namespace App.Commands.Resolver
             command.AddOption(instanciaOption);
             command.AddOption(limiteGeneracionesOption);
             command.AddOption(cantidadIndividuosOption);
-            command.SetHandler(EjecutarResolucion, instanciaOption, limiteGeneracionesOption, cantidadIndividuosOption);
+
+            command.SetHandler((rutaInstancia, limiteGeneraciones, cantidadIndividuos) =>
+            {
+                var parametros = new ParametrosSolucion(rutaInstancia, limiteGeneraciones, cantidadIndividuos);
+
+                var fileSystemHelper = FileSystemHelperFactory.Crear();
+                var lector = new LectorArchivoMatrizValoraciones(fileSystemHelper);
+
+                var consola = ConsoleProxyFactory.Crear();
+                var presentador = new Presentador(consola);
+
+                Handler(parametros, lector, presentador);
+
+#if DEBUG
+                Console.WriteLine("Presioná una tecla para salir...");
+                Console.ReadKey();
+#endif
+            }, instanciaOption, limiteGeneracionesOption, cantidadIndividuosOption);
 
             return command;
         }
 
-        private void EjecutarResolucion(string rutaInstancia, int limiteGeneraciones, int cantidadIndividuos)
+        internal static void Handler(ParametrosSolucion parametros, LectorArchivoMatrizValoraciones lector, Presentador presentador)
         {
-            var parametros = new ParametrosSolucion(rutaInstancia, limiteGeneraciones, cantidadIndividuos);
-
-            var fileSystemHelper = FileSystemHelperFactory.Crear();
-            var lector = new LectorArchivoMatrizValoraciones(fileSystemHelper);
             using var cts = new CancellationTokenSource();
-            ConfigurarCancelacion(cts);
+            ConfigurarCancelacion(cts, presentador);
 
             try
             {
@@ -58,31 +63,32 @@ namespace App.Commands.Resolver
                 var poblacion = PoblacionFactory.Crear(parametros.CantidadIndividuos, individuoFactory);
 
                 var algoritmoGenetico = new AlgoritmoGenetico(poblacion, parametros.LimiteGeneraciones);
-                ConfigurarProgreso(parametros, algoritmoGenetico);
+                ConfigurarProgreso(parametros, algoritmoGenetico, presentador);
 
                 var stopwatch = Stopwatch.StartNew();
                 var (mejorIndividuo, generaciones) = algoritmoGenetico.Ejecutar(cts.Token);
                 stopwatch.Stop();
 
-                MostrarResultado(mejorIndividuo, generaciones, stopwatch.ElapsedMilliseconds);
+                MostrarResultado(mejorIndividuo, generaciones, stopwatch.ElapsedMilliseconds, presentador);
             }
             catch (Exception ex)
             {
-                MostrarError(ex.Message);
+                MostrarError(ex.Message, presentador);
             }
         }
 
-        private void ConfigurarCancelacion(CancellationTokenSource cts)
+        private static void ConfigurarCancelacion(CancellationTokenSource cts, Presentador presentador)
         {
             Console.CancelKeyPress += (_, e) =>
             {
-                _presentador.MostrarAdvertencia("\nCancelación solicitada por el usuario.");
+                presentador.MostrarAdvertencia("\nCancelación solicitada por el usuario.");
                 e.Cancel = true;
                 cts.Cancel();
             };
         }
 
-        private void ConfigurarProgreso(ParametrosSolucion parametros, AlgoritmoGenetico algoritmoGenetico)
+        private static void ConfigurarProgreso(
+            ParametrosSolucion parametros, AlgoritmoGenetico algoritmoGenetico, Presentador presentador)
         {
             if (parametros.LimiteGeneraciones > 0)
             {
@@ -94,7 +100,7 @@ namespace App.Commands.Resolver
                     int progreso = generacion * tamañoBarraProgreso / parametros.LimiteGeneraciones;
                     string barraProgreso = new string('#', progreso).PadRight(tamañoBarraProgreso, '-');
                     string mensaje = $"[{barraProgreso}] {generacion}/{parametros.LimiteGeneraciones}";
-                    _presentador.MostrarProgreso(mensaje);
+                    presentador.MostrarProgreso(mensaje);
                 };
             }
             else
@@ -104,21 +110,21 @@ namespace App.Commands.Resolver
                     if (cancellationToken.IsCancellationRequested) return;
 
                     string mensaje = $"Procesando generación #{generacion}.";
-                    _presentador.MostrarProgreso(mensaje);
+                    presentador.MostrarProgreso(mensaje);
                 };
             }
         }
 
-        private void MostrarResultado(Individuo mejorIndividuo, int generaciones, long tiempoMs)
+        private static void MostrarResultado(Individuo mejorIndividuo, int generaciones, long tiempoMs, Presentador presentador)
         {
-            _presentador.MostrarExito($"\nResultado encontrado después de {generaciones} generaciones.");
-            _presentador.MostrarExito($"Resultado obtenido: {mejorIndividuo}.");
-            _presentador.MostrarExito($"Tiempo de ejecución: {tiempoMs} ms.");
+            presentador.MostrarExito($"\nResultado encontrado después de {generaciones} generaciones.");
+            presentador.MostrarExito($"Resultado obtenido: {mejorIndividuo}.");
+            presentador.MostrarExito($"Tiempo de ejecución: {tiempoMs} ms.");
         }
 
-        private void MostrarError(string mensaje)
+        private static void MostrarError(string mensaje, Presentador presentador)
         {
-            _presentador.MostrarError($"\nSe produjo un error: {mensaje}");
+            presentador.MostrarError($"\nSe produjo un error: {mensaje}");
         }
     }
 }
