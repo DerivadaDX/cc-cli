5f74a4c7cbb9becefbfe572fd1e9c8687e856f7d
DerivadaDX <derivadadx@gmail.com>
2025-06-16 21:53:09 -0300

    fix(resolver-command): se evita línea en blanco innecesaria tras cancelación (#56)
    En este PR se refina el comportamiento del `Presentador` para evitar
    imprimir líneas en blanco innecesarias luego de mostrar progreso o ante
    cancelaciones.
    Ahora el propio `Presentador` gestiona internamente los saltos de línea,
    evitando que los consumidores tengan que preocuparse por ese detalle, lo
    que permitió ajustar los mensajes en `ResolverCommand`.

--- src/App/Presentador.cs
+++ src/App/Presentador.cs
@@ -3,6 +3,7 @@
     internal class Presentador
     {
         private readonly ConsoleProxy _consola;
+        private bool _lineaEnCurso = false;
 
         internal Presentador(ConsoleProxy consola)
         {
@@ -12,22 +13,30 @@
 
         internal void MostrarInfo(string mensaje)
         {
+            AgregarSaltoSiNecesario();
             MostrarMensajeConColor(mensaje, ConsoleColor.White);
+            _lineaEnCurso = false;
         }
 
         internal void MostrarExito(string mensaje)
         {
+            AgregarSaltoSiNecesario();
             MostrarMensajeConColor(mensaje, ConsoleColor.Green);
+            _lineaEnCurso = false;
         }
 
         internal void MostrarAdvertencia(string mensaje)
         {
+            AgregarSaltoSiNecesario();
             MostrarMensajeConColor(mensaje, ConsoleColor.Yellow);
+            _lineaEnCurso = false;
         }
 
         internal void MostrarError(string mensaje)
         {
+            AgregarSaltoSiNecesario();
             MostrarMensajeConColor(mensaje, ConsoleColor.Red);
+            _lineaEnCurso = false;
         }
 
         internal void MostrarProgreso(string mensaje)
@@ -35,6 +44,13 @@
             _consola.ForegroundColor(ConsoleColor.White);
             _consola.Write($"\r{mensaje}");
             _consola.ResetColor();
+            _lineaEnCurso = true;
+        }
+
+        private void AgregarSaltoSiNecesario()
+        {
+            if (_lineaEnCurso)
+                _consola.WriteLine(string.Empty);
         }
 
         private void MostrarMensajeConColor(string mensaje, ConsoleColor color)
