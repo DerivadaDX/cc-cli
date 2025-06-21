e06327f6012a28e5c151bde527a4f497b7e0b781
DerivadaDX <derivadadx@gmail.com>
2025-06-21 20:42:04 -0300

    Uso consistente de llaves en namespaces.

--- tests/Solver.Tests/Individuos/IndividuoOptimizacionAsignacionesTests.cs
+++ tests/Solver.Tests/Individuos/IndividuoOptimizacionAsignacionesTests.cs
@@ -2,39 +2,40 @@ using Common;
 using NSubstitute;
 using Solver.Individuos;
 
-namespace Solver.Tests.Individuos;
-
-public class IndividuoOptimizacionAsignacionesTests : IDisposable
+namespace Solver.Tests.Individuos
 {
-    public void Dispose()
+    public class IndividuoOptimizacionAsignacionesTests : IDisposable
     {
-        GeneradorNumerosRandomFactory.SetearGenerador(null);
-        GC.SuppressFinalize(this);
-    }
-
-    [Fact]
-    public void Mutar_Asignaciones_SeAsignaDistribucionOptima()
-    {
-        var random = Substitute.For<GeneradorNumerosRandom>();
-        random.SiguienteDouble().Returns(1.0);
-        GeneradorNumerosRandomFactory.SetearGenerador(random);
-
-        var problema = InstanciaProblema.CrearDesdeMatrizDeValoraciones(new decimal[,]
+        public void Dispose()
         {
+            GeneradorNumerosRandomFactory.SetearGenerador(null);
+            GC.SuppressFinalize(this);
+        }
+
+        [Fact]
+        public void Mutar_Asignaciones_SeAsignaDistribucionOptima()
+        {
+            var random = Substitute.For<GeneradorNumerosRandom>();
+            random.SiguienteDouble().Returns(1.0);
+            GeneradorNumerosRandomFactory.SetearGenerador(random);
+
+            var problema = InstanciaProblema.CrearDesdeMatrizDeValoraciones(new decimal[,]
+            {
             { 100m, 1m, 2m, 3m },
             { 4m, 100m, 5m, 6m },
             { 7m, 8m, 100m, 9m },
             { 10m, 11m, 12m, 100m },
-        });
-        var cromosoma = new List<int> { 1, 2, 3, 4, 3, 2, 1 };
-        var individuo = new IndividuoOptimizacionAsignaciones(cromosoma, problema);
+            });
+            var cromosoma = new List<int> { 1, 2, 3, 4, 3, 2, 1 };
+            var individuo = new IndividuoOptimizacionAsignaciones(cromosoma, problema);
 
-        individuo.Mutar();
+            individuo.Mutar();
 
-        Assert.Equal(1, individuo.Cromosoma[3]);
-        Assert.Equal(2, individuo.Cromosoma[4]);
-        Assert.Equal(3, individuo.Cromosoma[5]);
-        Assert.Equal(4, individuo.Cromosoma[6]);
+            Assert.Equal(1, individuo.Cromosoma[3]);
+            Assert.Equal(2, individuo.Cromosoma[4]);
+            Assert.Equal(3, individuo.Cromosoma[5]);
+            Assert.Equal(4, individuo.Cromosoma[6]);
+        }
+
     }
-
 }
