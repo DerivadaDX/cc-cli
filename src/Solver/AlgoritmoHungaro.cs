89bad6ec6b88cfd23ce1c43da3a6a0229eac4ce6
DerivadaDX <derivadadx@gmail.com>
2025-11-10 20:40:44 -0300

    extracción de algoritmo húngaro a clase propia

---
+++ src/Solver/AlgoritmoHungaro.cs
@@ -0,0 +1,53 @@
+using HungarianAlgorithmSolver = HungarianAlgorithm.HungarianAlgorithm;
+
+internal class AlgoritmoHungaro
+{
+    public decimal[] EncontrarAsignacionQueMinimizaEnvidia(decimal[,] valoraciones)
+    {
+        int[,] matrizCostosEnteros = ConvertirValoracionesACostos(valoraciones);
+        int[] asignacionesEnteros = HungarianAlgorithmSolver.FindAssignments(matrizCostosEnteros);
+        decimal[] asignaciones = asignacionesEnteros.Select(x => (decimal)x).ToArray();
+        return asignaciones;
+    }
+
+    private int[,] ConvertirValoracionesACostos(decimal[,] valoraciones)
+    {
+        decimal valoracionMaxima = ObtenerValoracionMaxima(valoraciones);
+        int cantidadAtomos = valoraciones.GetLength(0);
+        int cantidadAgentes = valoraciones.GetLength(1);
+
+        int[,] costos = new int[cantidadAtomos, cantidadAgentes];
+        for (int indiceAtomo = 0; indiceAtomo < cantidadAtomos; indiceAtomo++)
+        {
+            for (int indiceAgente = 0; indiceAgente < cantidadAgentes; indiceAgente++)
+            {
+                decimal costo = valoracionMaxima - valoraciones[indiceAtomo, indiceAgente];
+
+                // TODO: Poder trabajar con decimal. Se pasa a int porque la biblioteca que usamos no soporta decimal.
+                int costoRedondeado = (int)Math.Round(costo * 1000m);
+                costos[indiceAtomo, indiceAgente] = costoRedondeado;
+            }
+        }
+
+        return costos;
+    }
+
+    private decimal ObtenerValoracionMaxima(decimal[,] valoraciones)
+    {
+        int cantidadAtomos = valoraciones.GetLength(0);
+        int cantidadAgentes = valoraciones.GetLength(1);
+
+        var valoracionMaxima = decimal.MinValue;
+        for (int indiceAtomo = 0; indiceAtomo < cantidadAtomos; indiceAtomo++)
+        {
+            for (int indiceAgente = 0; indiceAgente < cantidadAgentes; indiceAgente++)
+            {
+                decimal valorActual = valoraciones[indiceAtomo, indiceAgente];
+                if (valorActual > valoracionMaxima)
+                    valoracionMaxima = valorActual;
+            }
+        }
+
+        return valoracionMaxima;
+    }
+}
