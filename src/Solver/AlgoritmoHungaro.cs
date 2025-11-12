9337260e16b7362ac8b6e8822df8af2bc3d67905
DerivadaDX <derivadadx@gmail.com>
2025-11-12 07:32:09 -0300

    se agregó namespace a AlgoritmoHungaro

--- src/Solver/AlgoritmoHungaro.cs
+++ src/Solver/AlgoritmoHungaro.cs
@@ -1,53 +1,56 @@
 using HungarianAlgorithmSolver = HungarianAlgorithm.HungarianAlgorithm;
 
-internal class AlgoritmoHungaro
+namespace Solver
 {
-    public int[] CalcularAsignacionOptimaDePorciones(decimal[,] valoracionesDePorciones)
+    internal class AlgoritmoHungaro
     {
-        int[,] matrizCostosEnteros = ConvertirValoracionesACostos(valoracionesDePorciones);
-        int[] asignaciones = HungarianAlgorithmSolver.FindAssignments(matrizCostosEnteros);
-        return asignaciones;
-    }
-
-    private int[,] ConvertirValoracionesACostos(decimal[,] valoracionesDePorciones)
-    {
-        decimal valoracionMaxima = ObtenerValoracionMaxima(valoracionesDePorciones);
-        int cantidadPorciones = valoracionesDePorciones.GetLength(0);
-        int cantidadAgentes = valoracionesDePorciones.GetLength(1);
-
-        int[,] costos = new int[cantidadPorciones, cantidadAgentes];
-        for (int indicePorcion = 0; indicePorcion < cantidadPorciones; indicePorcion++)
+        public int[] CalcularAsignacionOptimaDePorciones(decimal[,] valoracionesDePorciones)
         {
-            for (int indiceAgente = 0; indiceAgente < cantidadAgentes; indiceAgente++)
-            {
-                decimal valoracion = valoracionesDePorciones[indicePorcion, indiceAgente];
-                decimal costo = valoracionMaxima - valoracion;
-
-                // TODO: Poder trabajar con decimal. Se pasa a int porque la biblioteca que usamos no soporta decimal.
-                int costoRedondeado = (int)Math.Round(costo * 1000m);
-                costos[indicePorcion, indiceAgente] = costoRedondeado;
-            }
+            int[,] matrizCostosEnteros = ConvertirValoracionesACostos(valoracionesDePorciones);
+            int[] asignaciones = HungarianAlgorithmSolver.FindAssignments(matrizCostosEnteros);
+            return asignaciones;
         }
 
-        return costos;
-    }
-
-    private decimal ObtenerValoracionMaxima(decimal[,] valoraciones)
-    {
-        int cantidadPorciones = valoraciones.GetLength(0);
-        int cantidadAgentes = valoraciones.GetLength(1);
-
-        var valoracionMaxima = decimal.MinValue;
-        for (int indicePorcion = 0; indicePorcion < cantidadPorciones; indicePorcion++)
+        private int[,] ConvertirValoracionesACostos(decimal[,] valoracionesDePorciones)
         {
-            for (int indiceAgente = 0; indiceAgente < cantidadAgentes; indiceAgente++)
+            decimal valoracionMaxima = ObtenerValoracionMaxima(valoracionesDePorciones);
+            int cantidadPorciones = valoracionesDePorciones.GetLength(0);
+            int cantidadAgentes = valoracionesDePorciones.GetLength(1);
+
+            int[,] costos = new int[cantidadPorciones, cantidadAgentes];
+            for (int indicePorcion = 0; indicePorcion < cantidadPorciones; indicePorcion++)
             {
-                decimal valorActual = valoraciones[indicePorcion, indiceAgente];
-                if (valorActual > valoracionMaxima)
-                    valoracionMaxima = valorActual;
+                for (int indiceAgente = 0; indiceAgente < cantidadAgentes; indiceAgente++)
+                {
+                    decimal valoracion = valoracionesDePorciones[indicePorcion, indiceAgente];
+                    decimal costo = valoracionMaxima - valoracion;
+
+                    // TODO: Poder trabajar con decimal. Se pasa a int porque la biblioteca que usamos no soporta decimal.
+                    int costoRedondeado = (int)Math.Round(costo * 1000m);
+                    costos[indicePorcion, indiceAgente] = costoRedondeado;
+                }
             }
+
+            return costos;
         }
 
-        return valoracionMaxima;
+        private decimal ObtenerValoracionMaxima(decimal[,] valoraciones)
+        {
+            int cantidadPorciones = valoraciones.GetLength(0);
+            int cantidadAgentes = valoraciones.GetLength(1);
+
+            var valoracionMaxima = decimal.MinValue;
+            for (int indicePorcion = 0; indicePorcion < cantidadPorciones; indicePorcion++)
+            {
+                for (int indiceAgente = 0; indiceAgente < cantidadAgentes; indiceAgente++)
+                {
+                    decimal valorActual = valoraciones[indicePorcion, indiceAgente];
+                    if (valorActual > valoracionMaxima)
+                        valoracionMaxima = valorActual;
+                }
+            }
+
+            return valoracionMaxima;
+        }
     }
 }
